c9fc8b03e4250111f6d6a7afb9f36b2ba02ae5aa
Herryanto Siatono <herryanto@gmail.com>
2015-08-14 00:39:22 +0800

    Add brew tmuxinator and vim

---
+++ brew.sh
@@ -0,0 +1,30 @@
+brew update
+
+brew install ack
+brew install carthage
+brew install cheat
+brew install colordiff
+brew install ctags
+brew install htop-osx
+brew install hub
+brew install imagemagick
+brew install jq
+brew install jsawk
+brew install jsonpp
+brew install libksba
+brew install libyaml
+brew install mongodb
+brew install mysql
+brew install node
+brew install postgresql
+brew install rbenv
+brew install reattach-to-user-namespace
+brew install rename
+brew install rlwrap
+brew install ruby-build
+brew install the_silver_searcher
+brew install tmux
+brew install tree
+brew install unrar
+brew install vim --override-system-vi
+brew install wget

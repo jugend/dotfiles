a64f42fba9aff0a9801f2369190fa26e897c8a77
Herryanto Siatono <hsiatono@paypal.com>
2017-07-05 10:08:50 +0800

    Update aliases, add yarn_install

---
+++ yarn_install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+npm install -g yarn
+yarn global add eslint babel-eslint eslint-config-standard eslint-config-standard-react eslint-plugin-promise eslint-plugin-react eslint-plugin-standard eslint-plugin-import eslint-plugin-node

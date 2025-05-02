6a74cc5706a83860b875bb3339a85ae10e810ce5
Herryanto Siatono <hsiatono@paypal.com>
2025-05-02 21:01:12 +0800

    Update formatter

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -36,8 +36,8 @@ return {
       -- Conform can also run multiple formatters sequentially
       python = { 'isort', 'black' },
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
-      javascriptreact = { 'eslint_d', stop_after_first = true },
+      javascript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
       -- typescript = { 'eslint_d', 'prettierd', 'prettier', stop_after_first = true },
     },
   },

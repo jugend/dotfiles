2a78165c370e2c4e3131b1d2a96c90f5da08a125
Herryanto Siatono <hsiatono@paypal.com>
2025-05-05 08:01:19 +0800

    Auto save js and update fzflua keymaps

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -24,8 +24,8 @@ return {
         -- Disable "fbrmat_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, javascript = true, javascriptreact = true, json = true }
-        -- local disable_filetypes = { c = true, cpp = true }
+        -- local disable_filetypes = { c = true, cpp = true, javascript = true, javascriptreact = true, json = true }
+        local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
